088f593ac7439a2045950c320d10da06562b5f6c
Fresh-timeey <aks25122005@gmail.com>
2024-12-03 12:26:45 +0300

    4 commite

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
+/* script.js */
+
 let percentage = 0;
+
 let interval = setInterval(() => {
     percentage += 1;
     document.getElementById("loading-text").innerText = `${percentage}%`;
 
+    
+    
     if (percentage === 100) {
         clearInterval(interval);
         document.getElementById("preloader").style.display = 'none'; // Прячем предзагрузчик
         document.getElementById("content").style.display = 'flex';  // Показываем основной контент
     }
 }, 50); // Обновление прогресса каждые 50 миллисекунд
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const music = document.getElementById('bg-music');
+    const musicIcon = document.getElementById('music-icon');  // Иконка изображения
+    
+    document.getElementById('toggle-music').addEventListener('click', () => {
+        if (music.paused) {
+            music.play();
+            musicIcon.src = 'audio_on.png'; // Изменение на "включённая музыка"
+        } else {
+            music.pause();
+            musicIcon.src = 'audio_out.png'; // Изменение на "выключённая музыка"
+        }
+    });
+});
+
+
+
+
+
